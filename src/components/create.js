f9329597b31c4414d32e287bf14d40ecb99f2b60
Shreeram-sarang <shreeram.cs21@bmsce.ac.in>
2024-03-26 16:30:53 +0530

    fivjsfnvjf

---
+++ src/components/create.js
@@ -0,0 +1,13 @@
+// create.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Create() {
+    return (  
+        <>
+            <Link to='/login'><button>Create</button></Link>
+        </>
+    );
+}
+
+export default Create;
